31a85ef7b30f595a0d75a3c60f2ea81be214378d
Mathis <msjemenblc@gmail.com>
2024-08-05 16:49:07 +0200

    project starter

---
+++ src/main/java/com/apichatop/ApichatopApplication.java
@@ -0,0 +1,13 @@
+package com.apichatop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApichatopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApichatopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apichatop/ApichatopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apichatop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApichatopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
